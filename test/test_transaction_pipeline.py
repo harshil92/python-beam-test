8989a2b01f279dec5111601af3c6023517889d97
unknown <hdilipcumar@hotmail.com>
2022-04-04 10:39:34 +0100

    refactored ValidateRow and added more checks

--- test/test_transaction_pipeline.py
+++ test/test_transaction_pipeline.py
@@ -3,13 +3,16 @@ import logging
 import os
 import unittest
 
-import apache_beam as beam
 from apache_beam.io import ReadFromText
 from apache_beam.testing.test_pipeline import TestPipeline
 from apache_beam.testing.util import assert_that, equal_to
 
-from main import run, ComputeTransactions, ValidateRow
+from main import run, ComputeTransactions
 
+E2E_TEST_DATA_PATH = os.path.join(os.getcwd(), 'test/resources/e2e-test-data.csv')
+INVALID_ROW_TEST_DATA_PATH = os.path.join(os.getcwd(), 'test/resources/e2e-test-invalid-row.csv')
+INVALID_TIMESTAMP_TEST_DATA_PATH = os.path.join(os.getcwd(), 'test/resources/e2e-test-invalid-timestamp.csv')
+TEST_RESULT_PATH = os.path.join(os.getcwd(), 'output/test_result')
 
 class TestTransactionPipeline(unittest.TestCase):
 
@@ -17,8 +20,8 @@ class TestTransactionPipeline(unittest.TestCase):
         expected_result = [b'date, total_amount\n', b'2017-03-18, 2102.22\n', b'2017-08-31, 13700000023.08\n',
                            b'2018-02-27, 129.12\n']
 
-        run(['--input=%s' % os.path.join(os.getcwd(), 'test/resources/e2e-test-data.csv'),
-             '--output=%s' % os.path.join(os.getcwd(), 'output/test_result')],
+        run(['--input=%s' % E2E_TEST_DATA_PATH,
+             '--output=%s' % TEST_RESULT_PATH],
             save_main_session=False)
 
         with gzip.open(os.path.join(os.getcwd(), 'output/test_result.csv.gz'), 'r') as f:
@@ -30,8 +33,8 @@ class TestTransactionPipeline(unittest.TestCase):
         expected_result = [b'date, total_amount\n', b'2017-03-18, 2102.22\n',
                            b'2018-02-27, 129.12\n']
 
-        run(['--input=%s' % os.path.join(os.getcwd(), 'test/resources/e2e-test-invalid-row.csv'),
-             '--output=%s' % os.path.join(os.getcwd(), 'output/test_result')],
+        run(['--input=%s' % INVALID_ROW_TEST_DATA_PATH,
+             '--output=%s' % TEST_RESULT_PATH],
             save_main_session=False)
 
         with gzip.open(os.path.join(os.getcwd(), 'output/test_result.csv.gz'), 'r') as f:
@@ -43,26 +46,9 @@ class TestTransactionPipeline(unittest.TestCase):
         expected_result = [('2017-03-18', 2102.22), ('2017-08-31', 13700000023.08), ('2018-02-27', 129.12)]
 
         with TestPipeline() as p:
-            input_lines = p | ReadFromText(os.path.join(os.getcwd(), 'test/resources/e2e-test-data.csv'),
+            input_lines = p | ReadFromText(E2E_TEST_DATA_PATH,
                                      skip_header_lines=True)
             output = input_lines | ComputeTransactions()
-
-            print(output)
-            assert_that(
-                output,
-                equal_to(expected_result))
-
-    def test_validate_row(self):
-        expected_result = ['2009-01-09 02:54:25 UTC,wallet00000e719adfeaa64b5a,wallet00001866cb7e0f09a890,1021101.99',
-                           '2017-01-01 04:22:23 UTC,wallet00000e719adfeaa64b5a,wallet00001e494c12b3083634,19.95',
-                           '2017-03-18 14:09:16 UTC,wallet00001866cb7e0f09a890,wallet00001e494c12b3083634,2102.22',
-                           '2017-03-18 14:10:44 UTC,wallet00001866cb7e0f09a890,wallet00000e719adfeaa64b5a,1.00030',
-                           '2018-02-27 16:04:11 UTC,wallet00005f83196ec58e4ffe,wallet00001866cb7e0f09a890,129.12']
-
-        with TestPipeline() as p:
-            input_lines = p | ReadFromText(os.path.join(os.getcwd(), 'test/resources/e2e-test-invalid-row.csv'),
-                                     skip_header_lines=True)
-            output = input_lines | beam.ParDo(ValidateRow())
 
             print(output)
             assert_that(
