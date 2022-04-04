7da58d498f0126af9732da6e6dccf19f6fc1b9b4
unknown <hdilipcumar@hotmail.com>
2022-04-04 11:07:48 +0100

    added decode method

--- test/test_transaction_pipeline.py
+++ test/test_transaction_pipeline.py
@@ -14,11 +14,12 @@ INVALID_ROW_TEST_DATA_PATH = os.path.join(os.getcwd(), 'test/resources/e2e-test-
 INVALID_TIMESTAMP_TEST_DATA_PATH = os.path.join(os.getcwd(), 'test/resources/e2e-test-invalid-timestamp.csv')
 TEST_RESULT_PATH = os.path.join(os.getcwd(), 'output/test_result')
 
+
 class TestTransactionPipeline(unittest.TestCase):
 
     def test_e2e_pipeline(self):
-        expected_result = [b'date, total_amount\n', b'2017-03-18, 2102.22\n', b'2017-08-31, 13700000023.08\n',
-                           b'2018-02-27, 129.12\n']
+        expected_result = ['date, total_amount\n', '2017-03-18, 2102.22\n', '2017-08-31, 13700000023.08\n',
+                           '2018-02-27, 129.12\n']
 
         run(['--input=%s' % E2E_TEST_DATA_PATH,
              '--output=%s' % TEST_RESULT_PATH],
@@ -26,12 +27,13 @@ class TestTransactionPipeline(unittest.TestCase):
 
         with gzip.open(os.path.join(os.getcwd(), 'output/test_result.csv.gz'), 'r') as f:
             lines = f.readlines()
+            u_lines = listDecode(lines)
 
-        self.assertEqual(lines, expected_result)
+        self.assertEqual(u_lines, expected_result)
 
     def test_e2e_pipeline_invalid_row(self):
-        expected_result = [b'date, total_amount\n', b'2017-03-18, 2102.22\n',
-                           b'2018-02-27, 129.12\n']
+        expected_result = ['date, total_amount\n', '2017-03-18, 2102.22\n',
+                           '2018-02-27, 129.12\n']
 
         run(['--input=%s' % INVALID_ROW_TEST_DATA_PATH,
              '--output=%s' % TEST_RESULT_PATH],
@@ -39,21 +41,29 @@ class TestTransactionPipeline(unittest.TestCase):
 
         with gzip.open(os.path.join(os.getcwd(), 'output/test_result.csv.gz'), 'r') as f:
             lines = f.readlines()
+            u_lines = listDecode(lines)
 
-        self.assertEqual(lines, expected_result)
+        self.assertEqual(u_lines, expected_result)
 
     def test_compute_transactions(self):
         expected_result = [('2017-03-18', 2102.22), ('2017-08-31', 13700000023.08), ('2018-02-27', 129.12)]
 
         with TestPipeline() as p:
             input_lines = p | ReadFromText(E2E_TEST_DATA_PATH,
-                                     skip_header_lines=True)
+                                           skip_header_lines=True)
             output = input_lines | ComputeTransactions()
 
             print(output)
             assert_that(
                 output,
                 equal_to(expected_result))
+
+
+def listDecode(lines):
+    u_lines = []
+    for line in lines:
+        u_lines.append(line.decode('utf-8'))
+    return u_lines
 
 
 if __name__ == '__main__':
