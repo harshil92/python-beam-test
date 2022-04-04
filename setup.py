86eb2d32c525beed359ef359951c0e53852f9172
unknown <hdilipcumar@hotmail.com>
2022-04-04 10:50:50 +0100

    rename package

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 import setuptools
 REQUIRED_PACKAGES = []
 
-PACKAGE_NAME = 'virgin_data_eng_test'
+PACKAGE_NAME = 'python_beam_test'
 PACKAGE_VERSION = '0.0.1'
 
 setuptools.setup(
     name=PACKAGE_NAME,
     version=PACKAGE_VERSION,
-    description='This pipeline processes transaction data',
+    description='This pipeline processes transaction data.',
     install_requires=REQUIRED_PACKAGES,
     packages=setuptools.find_packages(),
     package_data= {
